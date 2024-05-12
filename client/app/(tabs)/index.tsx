020bf3d1da5d7c2a45727ce34a80c88987d4c716
lampealex888 <lampealex888@gmail.com>
2024-05-12 19:51:12 -0400

    added welcome page

--- client/app/(tabs)/index.tsx
+++ client/app/(tabs)/index.tsx
@@ -1,23 +1,36 @@
-import React from 'react';
-import { View, Text, Button, StatusBar } from 'react-native';
+import React from "react";
+import { View, Text, Button, StatusBar, Image } from "react-native";
+import { vars } from "nativewind";
+
+const userTheme = vars({
+  "--color-primary": "255 0 0",
+});
 
 export default function App() {
   return (
     <View className="flex-1 bg-white items-center justify-center px-5">
       <StatusBar barStyle="dark-content" />
-      <View className="grow items-center justify-center">
-        <Text className="text-[80px] text-[#34C759]">🌲</Text>
-        <Text className="text-2xl font-bold my-2">Forevergreen</Text>
+      <View className="grow items-center justify-center gap-24">
+        <Text className="text-5xl font-bold my-2">
+          Forever<Text className="text-[#59A56D]">green</Text>
+        </Text>
+        <Image
+          className="w-[48rem] h-96"
+          source={require("../../assets/images/tree-logo.png")}
+        />
       </View>
       <View className="w-full mb-8">
         <Button
-          onPress={() => alert('Get Started')}
+          onPress={() => alert("Get Started")}
           title="Get Started"
-          color="#34C759"
+          color="#409858"
         />
         <Text className="mt-4 text-center text-base">
-          Already helping our planet?{' '}
-          <Text className="font-bold text-blue-500" onPress={() => alert('Log In')}>
+          Already helping our planet?{" "}
+          <Text
+            className="font-bold text-blue-500"
+            onPress={() => alert("Log In")}
+          >
             Log in
           </Text>
         </Text>
