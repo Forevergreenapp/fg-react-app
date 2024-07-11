295307405ad03ff472cb8306cdc8c7e785d432fe
lampealex888 <lampealex888@gmail.com>
2024-07-11 12:26:12 -0400

    Update diet.tsx

--- frontend/app/(carbon-calculator)/diet.tsx
+++ frontend/app/(carbon-calculator)/diet.tsx
@@ -1,32 +1,24 @@
 import React, { useState, useEffect } from "react";
-import { View, Text, ScrollView, TouchableOpacity } from "react-native";
+import { View, Text, ScrollView } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { useTheme, RadioButton } from "react-native-paper";
-import * as Progress from "react-native-progress";
-import Icon from "react-native-vector-icons/FontAwesome";
-import { router } from "expo-router";
+import {
+  Header,
+  RadioButtonGroup,
+  NextButton,
+} from "../../components/carbon-calculator";
 
 export default function DietCalculator() {
-  const theme = useTheme();
-  const [diet, setDiet] = useState("");
+  const [diet, setDiet] = useState("Average");
   const [dietEmissions, setDietEmissions] = useState(0.0);
   const [isFormValid, setIsFormValid] = useState(false);
   const [progress, setProgress] = useState(0.33);
-
-  const dietOptions = [
-    "Meat Lover",
-    "Average",
-    "No Beef Or Lamb",
-    "Veterinarian",
-    "Vegan",
-  ];
 
   useEffect(() => {
     setIsFormValid(diet !== "");
 
     switch (diet) {
       case "Meat Lover":
-        setDietEmissions(3.5);
+        setDietEmissions(3.3);
         break;
       case "Average":
         setDietEmissions(2.5);
@@ -51,52 +43,28 @@ export default function DietCalculator() {
     }
   }, [diet]);
 
-  const handleDietSelection = (selectedDiet: string) => {
-    setDiet(selectedDiet);
-  };
-
   return (
     <ScrollView contentContainerStyle={{ flexGrow: 1 }}>
       <SafeAreaView>
         <View className="px-12">
           {/* Header */}
-          <View className="flex-row items-center gap-6 mt-4">
-            <Icon
-              name="arrow-left"
-              size={24}
-              color="black"
-              onPress={() => router.replace("transportation")}
-            />
-            <View className="w-5/6">
-              <Progress.Bar
-                progress={progress}
-                width={null}
-                color="#AEDCA7"
-                unfilledColor="#FFF"
-                borderColor={theme.colors.onBackground}
-                borderWidth={1.5}
-                height={15}
-                borderRadius={9999}
-              />
-            </View>
-          </View>
-          <Text className="text-4xl mt-6">Diet</Text>
+          <Header onBack="transportation" progress={progress} title="Diet" />
 
           {/* Diet Selection */}
-          <Text className="mt-8 text-xl">Select your Diet</Text>
-          <View className="-ml-2 mt-3">
-            {dietOptions.map((option) => (
-              <View key={option} className="flex-row items-center">
-                <RadioButton
-                  value={option}
-                  status={diet === option ? "checked" : "unchecked"}
-                  onPress={() => handleDietSelection(option)}
-                  color="#44945F"
-                />
-                <Text className="text-lg">{option}</Text>
-              </View>
-            ))}
-          </View>
+          <RadioButtonGroup
+            question="Select your Diet"
+            options={[
+              "Meat Lover",
+              "Average",
+              "No Beef Or Lamb",
+              "Veterinarian",
+              "Vegan",
+            ]}
+            value={diet}
+            onChange={(selectedDiet: string) => {
+              setDiet(selectedDiet);
+            }}
+          />
 
           {/* Emissions Display */}
           <View className="mt-8 mb-16">
@@ -112,29 +80,7 @@ export default function DietCalculator() {
         </View>
 
         {/* Next Button */}
-        <View className="flex items-end mb-10 mr-10">
-          <TouchableOpacity
-            onPress={() => {
-              if (isFormValid) {
-                router.replace("energy");
-              }
-            }}
-          >
-            <View
-              className={`py-3 px-4 rounded-full border-2 h-16 w-16 ${
-                isFormValid
-                  ? "border-black bg-[#AEDCA7]"
-                  : "border-gray-300 bg-gray-300"
-              }`}
-            >
-              <Icon
-                name="arrow-right"
-                size={30}
-                color={isFormValid ? "#000" : "#AAA"}
-              />
-            </View>
-          </TouchableOpacity>
-        </View>
+        <NextButton isFormValid={isFormValid} onNext={"energy"} />
       </SafeAreaView>
     </ScrollView>
   );
