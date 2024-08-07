bd6913d344295e33fb96298e24ee2d2cfbd8a821
lampealex888 <lampealex888@gmail.com>
2024-08-06 23:49:16 -0400

    add html file, change npm run <device> script, add metro runtime

--- frontend/constants/images.ts
+++ frontend/constants/images.ts
@@ -1,2 +1,4 @@
-export const Earth = require("@/assets/images/earth.png");
-export const FGCoin = require("@/assets/images/fg-coin.png");
+const Earth = require("../assets/images/earth.png");
+const FGCoin = require("../assets/images/fg-coin.png");
+
+export { Earth, FGCoin };
